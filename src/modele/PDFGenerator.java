1b71021210edc9932171d6f9c2726261f0b10904
DeborahPlop <deborahgelin@hotmail.fr>
2019-03-18 16:30:06 +0100

    gestionSessiontableau

---
+++ src/modele/PDFGenerator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.io.IOException;
+import java.io.FileOutputStream;
+///import com.itextpdf.text.*;
+
+
+/**
+ *
+ * @author debor
+ */
+public class PDFGenerator 
+{
+    public static final String url="C:\\Users\\debor\\Desktop\\BTS\\2eme annee\\PPE\\Applis_Support\\2018\\Appli_Java_JavaFX\\pdftest.pdf";
+    
+    /*public static void main(String[] args)throws DocumentException, IOException
+    {
+        Document document= new Document();
+        {
+            
+        }
+    }*/
+    
+}
